e60f4beb2c3334bfd2d1a8c927ad02dd3e5846d8
SamoHammer <samir.gabriel@gmail.com>
2025-09-20 21:52:02 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-// Top-level Gradle build file
-// On déclare ici les versions des plugins, puis on les applique dans le module :app
-
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
