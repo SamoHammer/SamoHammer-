41bf1a2bb1117577ea8a4bdc87a20bce16b8c808
SamoHammer <samir.gabriel@gmail.com>
2025-09-20 21:27:47 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.1") // ✅ Extended FAB support
+    implementation("androidx.compose.material3:material3:1.1.2") // ✅ version stable trouvée sur MavenCentral
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
